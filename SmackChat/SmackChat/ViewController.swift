ea6e99df6b8c6fa0e68a3882073ef7cc74546aff
Ramilia Imankulova <ramiliaimankulova@Ramilias-MacBook-Pro.local>
2018-10-23 01:32:51 +0600

    first page created and pods are added

---
+++ SmackChat/SmackChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackChat
+//
+//  Created by Ramilia Imankulova on 10/23/18.
+//  Copyright © 2018 Ramilia Imankulova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
